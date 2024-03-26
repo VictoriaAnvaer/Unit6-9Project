3864cbdcc0a3c1a903c8f339f5bbe253ce7f307a
BT_2N2_32 <sample@bths.edu>
2024-03-26 13:54:40 -0400

    Multiple boards at once

--- src/Board.java
+++ src/Board.java
@@ -3,7 +3,7 @@ public class Board {
     private String name;
     private Space[][] grid;
     private boolean isWinner;
-    private ArrayList<Integer> drawNum;
+    public static ArrayList<Integer> drawNum = new ArrayList<>();
     Board(String name) {
         this.name = name;
         //setting up grid
@@ -21,12 +21,61 @@ public class Board {
             }
             low+=15;
         }
-        drawNum = new ArrayList<>();
     }
     public boolean getIsWinner() {
         return isWinner;
     }
-    public void checkWinner() { // MAKE MORE EFFICIENT
+    public String getName() {
+        return name;
+    }
+    public static int drawNum() {
+        int num = (int) ( Math.random() * 74) + 1;
+        for (int i = 0; i < drawNum.size(); i++) {
+            if (drawNum.get(i) == num) {
+                num = (int) ( Math.random() * 74) + 1;
+                i = 0;
+            }
+        }
+        drawNum.add(num);
+        return num;
+    }
+    public void findDrawnNum(int num) {
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[r].length; c++) {
+                if (grid[r][c] instanceof NumberSpace) {
+                    if (((NumberSpace) grid[r][c]).getNum() == num) {
+                        grid[r][c] = new TakenSpace(grid[r][c].getSpace());
+                        checkWinner();
+                    }
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        String printBoard = name + "\n" + Util.ANSI_BLACK_BACKGROUND + " B  I  N  G  O " + Util.ANSI_RESET + "\n";
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[r].length; c++) {
+                printBoard+= grid[r][c] + " ";
+                if (grid[r][c].getSpace().length() == 1) {
+                    printBoard+=" ";
+                }
+            }
+            printBoard+= Util.ANSI_RESET + "\n";
+        }
+        return printBoard;
+    }
+    private static int[] ranNum(int low) {
+        int[] ranArray = new int[15];
+        for (int i = 0; i < ranArray.length; i++) {
+            ranArray[i] = i + low;
+        }
+        Util.shuffle(ranArray);
+        return ranArray;
+    }
+    private void checkWinner() { // MAKE MORE EFFICIENT
         // check each row
         for (int r = 0; r < grid.length; r++) {
             int taken = 0;
@@ -69,55 +118,6 @@ public class Board {
         if (taken == 5 || taken1 == 5) {
             isWinner = true;
         }
-    }
-    public void findDrawnNum() {
-        int num = drawNum();
-        System.out.println("Drawn number was: " + num);
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[r].length; c++) {
-                if (grid[r][c] instanceof NumberSpace) {
-                    if (((NumberSpace) grid[r][c]).getNum() == num) {
-                        grid[r][c] = new TakenSpace(grid[r][c].getSpace());
-                    }
-
-                }
-            }
-        }
-
-    }
-
-    @Override
-    public String toString() {
-        String printBoard = Util.ANSI_BLACK_BACKGROUND + " B  I  N  G  O " + Util.ANSI_RESET + "\n";
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[r].length; c++) {
-                printBoard+= grid[r][c] + " ";
-                if (grid[r][c].getSpace().length() == 1) {
-                    printBoard+=" ";
-                }
-            }
-            printBoard+= Util.ANSI_RESET + "\n";
-        }
-        return printBoard;
-    }
-    private static int[] ranNum(int low) {
-        int[] ranArray = new int[15];
-        for (int i = 0; i < ranArray.length; i++) {
-            ranArray[i] = i + low;
-        }
-        Util.shuffle(ranArray);
-        return ranArray;
-    }
-    private int drawNum() {
-        int num = (int) ( Math.random() * 74) + 1;
-        for (int i = 0; i < drawNum.size(); i++) {
-            if (drawNum.get(i) == num) {
-                num = (int) ( Math.random() * 74) + 1;
-                i = 0;
-            }
-        }
-        drawNum.add(num);
-        return num;
     }
 }
 
--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Game {
     private static final Scanner SCANNER = new Scanner(System.in);
     private Board board;
-    //private Board[][] boardList; // if player wants multiple boards or multiple players
+    private ArrayList<Board> boardList; // if player wants multiple boards or multiple players
+    private boolean game;
     public Game() {
+        boardList = new ArrayList<>();
+        game = true;
     }
     public void start() {
         welcomePlayer();
@@ -11,19 +15,41 @@ public class Game {
     }
     private void welcomePlayer() {
         System.out.println("-------WELCOME-------");
-        System.out.print("Enter your name: ");
-        String name = SCANNER.nextLine();
-        board = new Board(name);
+        System.out.print("How many players would you like? ");
+        int numPlayer = SCANNER.nextInt();
+        SCANNER.nextLine();
+        for (int i = 0; i < numPlayer; i++) {
+            System.out.print("Enter player " + (i + 1) + " name: ");
+            String name = SCANNER.nextLine();
+            board = new Board(name);
+            boardList.add(board);
+        }
     }
     private void drawNumbers() {
-        while (!board.getIsWinner()) {
-            System.out.println(board);
+        while (game) {
+            System.out.println();
+            for (int i = 0; i < boardList.size(); i++) {
+                System.out.println(boardList.get(i));
+            }
             System.out.println("ENTER when you are ready to draw next number");
             SCANNER.nextLine();
-            board.findDrawnNum();
-            board.checkWinner();
+            int drawnNum = Board.drawNum();
+            System.out.println("Drawn number was: " + drawnNum);
+            for (int i = 0; i < boardList.size(); i++) {
+                boardList.get(i).findDrawnNum(drawnNum);
+                if (boardList.get(i).getIsWinner()) {
+                    //System.out.print(boardList.get(i).getName() + "'s board has won. Would you like to continue? [y/n] ");
+                    //String con = SCANNER.nextLine();
+                    //if (con.equals("n")) {
+                        game = false;
+                    //} FIX THIS PART SO THAT IT DOES NOT ASK EVERYTIME
+                }
+            }
         }
-        System.out.println(board);
-        System.out.println("Congratulations! you have won.");
+        for (int i = 0; i < boardList.size(); i++) {
+            System.out.println(boardList.get(i));
+            System.out.println();
+        }
+        System.out.println("Congratulations! Your board has won.");
     }
 }
