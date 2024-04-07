029fc4b4bf648540ec312596cd6c96b499469cf1
Victoria <victoriaa127@nycstudents.net>
2024-04-07 16:57:29 -0400

    FINAL

--- src/TakenSpace.java
+++ src/TakenSpace.java
@@ -2,6 +2,7 @@ public class TakenSpace extends Space {
     TakenSpace(String space) {
         super(space);
     }
+    @Override
     public String toString() {
         return Util.ANSI_RED_BACKGROUND + super.toString() + Util.ANSI_WHITE_BACKGROUND;
     }
